1c507e5e457fd07b37f8b239bec12a085b767a90
Rinkesh-P <rinkesh_2000@hotmail.com>
2024-05-14 19:19:05 +1200

    Templates, views and URLS along with the creation of the static files are complete.
    - All html and css files have been created.
    - App has been configured
    
    For next session:
    
    - Working on main.html and fleshing out the webpage for it.
    - Adding the general front end design idea.

---
+++ SunnybraeWeb/store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('',views.product, name="product"),
+    path('cart/',views.cart, name="cart"),
+    path('faq/',views.faq, name="faq")
+]
--- SunnybraeWeb/store/views.py
+++ SunnybraeWeb/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def product(request):
+    context = {}
+    return render (request, 'store/product.html', context)
+
+def faq(request):
+    context = {}
+    return render (request, 'store/faq.html', context)
+
+def cart(request):
+    context = {}
+    return render (request, 'store/cart.html', context)
