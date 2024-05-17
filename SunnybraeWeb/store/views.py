3245fcbdff6b34bb4912719fbd040181775b8fd8
Rinkesh-P <rinkesh_2000@hotmail.com>
2024-05-17 15:13:36 +1200

    Limiting the product page to display x number of products
    Via the use of Paginator i can split the product page into multiple pages so that all 1000 products arent displayed all at one but rather it will be displayed via x number of products at a time.

--- SunnybraeWeb/store/views.py
+++ SunnybraeWeb/store/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import * 
+from django.core.paginator import Paginator #Paginator should divide the product page so that it shows x amount of products per page rather than all the products on the page
 # Create your views here.
 def product(request):
     products = Product.objects.all()
-    context = {'products':products}
+    
+    paginator = Paginator(products, 6) #limits it to x products per page #setting to 6 for testing purposes 
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+        
+    #context = {'products':products} 
+    context = {'page_object':page_object} #should render the page with x products as opposed to all products 
+    
     return render (request, 'store/product.html', context)
 
 def faq(request):
