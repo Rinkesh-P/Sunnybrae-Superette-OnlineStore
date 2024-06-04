e3e0caccf46c928d8003bf9e27abe60093fe8816
Rinkesh-P <rinkesh_2000@hotmail.com>
2024-06-04 14:47:27 +1200

    Created the CheckoutInfo in models.py
    Created the model for the CheckoutPage.

--- SunnybraeWeb/store/models.py
+++ SunnybraeWeb/store/models.py
@@ -54,3 +54,13 @@ class OrderItem(models.Model):
         return total 
 
 
+class CheckoutInfo(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null = False)
+    city = models.CharField(max_length=200, null = False)
+    suburb = models.CharField(max_length=200, null = False)
+    zipcode = models.CharField(max_length=200, null = False)
+    country = models.CharField(max_length=200, null = False)
+    date_added = models.DateTimeField(auto_now_add=True)
+     
