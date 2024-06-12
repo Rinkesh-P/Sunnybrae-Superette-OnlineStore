619e08a5f0fc07508346a151c6aa6753c59558bf
Rinkesh-P <rinkesh_2000@hotmail.com>
2024-06-12 15:45:29 +1200

    Added some comments to the javaScript code and removed LoadMore functionality.
    Added Comments to main.js to further describe the functionality of the code present and removed the LoadMore code as the Load more button  has been removed now.

--- SunnybraeWeb/static/js/main.js
+++ SunnybraeWeb/static/js/main.js
@@ -1,48 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     
-    //console.log("Dom fully loaded"); //check if dom is loaded. 
 
-    let page = 1;
-
-    function loadMoreProducts() {
-         page++;
-
-         fetch(`?page=${page}`)
-              .then((response) => {
-                   // Use Fetch API to make the request
-                   if (!response.ok) {
-                        throw new Error("Network response was not ok " + response.statusText);
-                   }
-                   return response.text();
-              })
-              .then((data) => {
-                   const parser = new DOMParser();
-                   const htmlDocument = parser.parseFromString(data, "text/html");
-
-                   const newProducts = htmlDocument.querySelectorAll(".product-item"); // Find all new product items in the response
-
-                   const container = document.getElementById("product-container"); // Get the product container
-
-                   newProducts.forEach((product) => container.appendChild(product)); // Add new products to the container
-
-                   const newPageObj = htmlDocument.querySelector(".pagination"); // Find out if there are more pages of products
-                   if (!newPageObj) {
-                        document.getElementById("load-more").style.display = "none";
-                   }
-              })
-              .catch((error) => { 
-                console.error("There has been a problem with your fetch operation:", error); 
-              });
-    }
-
-    //document.getElementById("load-more").addEventListener("click", loadMoreProducts); //wont work anymore as when run it will return null due to the script trying to find the element before it exists in the DOM
-
-    const loadMoreBtn = document.getElementById("load-more"); //find load more button 
-    if (loadMoreBtn) { //if the button is not null 
-        loadMoreBtn.addEventListener("click", loadMoreProducts);
-    }
-
-    document.addEventListener("click", function(event){
+    document.addEventListener("click", function(event){ //For when the user clicks on the add button on the product.html page 
         if (event.target && event.target.classList.contains("add-btn")) {
             const productId = event.target.getAttribute("data-product");
             const action = 'add';
@@ -50,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function(){
             updateUserOrder(productId, action);
         }
 
-       if (event.target && event.target.classList.contains("chg-quantity")) {
+       if (event.target && event.target.classList.contains("chg-quantity")) { //for when the user clicks on the increase/decrease arrows in the cart.html page
             const productId = event.target.getAttribute("data-product");
             const action = event.target.getAttribute("data-action");
             //console.log (productId, action);  // check if arrows are working 
@@ -58,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
-    function updateUserOrder(productId, action) { //CSRF token from the 'X-Csrftoken' HTTP header has incorrect length  
+     function updateUserOrder(productId, action) { //CSRF token from the 'X-Csrftoken' HTTP header has incorrect length  
+     
+          // When user clicks on the add/remove buttons this function will run which will provide the updateItem function in views.py with the 
+          // required information to make changes to the cart. 
 
         const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]'); // CSRF Token is already in html, but have to read in JS 
         if (!csrfToken){
@@ -81,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function(){
              });
      }
 
-     document.getElementById('reset').addEventListener('click', function(){
+     document.getElementById('reset').addEventListener('click', function(){ //reset button on the product page to reset any searchs made
           window.location.href = this.getAttribute('data-url'); 
      });
      
