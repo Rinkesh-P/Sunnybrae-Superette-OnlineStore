76f73cb82decc2918222168f81c85e428ced0030
Rinkesh-P <rinkesh_2000@hotmail.com>
2024-06-25 18:45:23 +1200

    changed the main page to be faq.html

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.product, name='product'),
+    path('faq/',views.faq, name='faq'),
+    path('product/',views.product, name="product"),
     path('cart/',views.cart, name='cart'),
-    path('faq/',views.faq, name="faq"),
+    #path('faq/',views.faq, name="faq"),
     path('register/',views.user_register, name="register"),
     path('login/',views.user_login, name="login"),
     path('updateItem/', views.updateItem, name='updateItem'),
